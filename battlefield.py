4dafa9e12f63e3d7cc780ded57550496a0339c33
n-rdy <giuseppegelardi@icloud.com>
2021-07-15 23:05:58 -0500

    began building the battlefield

--- battlefield.py
+++ battlefield.py
@@ -9,31 +9,57 @@ class Battlefield:
 
     def run_game(self):
         print("inside the run_game method!")
+        self.display_welcome()
 
 
     def display_welcome(self):
         print("Welcome to Robots vs Dinosaurs!")
-        self.initiate_game = input("Would you like to start the game?: 'yes' or 'no' ")
+        self.initiate_game = input("Would you like to start the game?: 'yes' or 'no': ")
+        if self.initiate_game == "yes":
+            self.battle()
+        if self.initiate_game == "no":
+            self.run_game()
         
 
     def battle(self):
-        self.fleet.robot_list[0].attack(self.herd.dino_list[0])
+        print("BATTLE!!")
+        self.battle_start = input("Who goes first? robots or dinosaurs?: ")
+        if self.battle_start == "robots":
+            print("ROBOTS ATTACK FIRST")
+            self.dino_turn(self)
+        if self.battle_start == "dinosaurs":
+            print("DINOSAURS ATTACK FIRST!")
+            self.robot_turn(self)
+
+
+        # self.fleet.robot_list[0].attack(self.herd.dino_list[0])
+        # self.herd.dino_list[0].attack(self.fleet.robot_list[0])
+
 
 
     def dino_turn(self, dinosaur):
+        # TODO find a way to alternate dino / robot turns
         pass
 
 
     def robot_turn(self, dinosaur):
+        # TODO find a way to alternate dino / robot turns
         pass
 
     def show_dino_opponent_options(self):
-        pass
+        print("something")
+
 
     def show_robot_opponent_options(self):
-        pass
+        print("something else idk")
+
 
     def display_winners(self):
-        pass
-            
+        if len(self.fleet.robot_list) == 0:
+            print("DINOSAURS WIN")
+        if len(self.herd.dino_list) == 0:
+            print("ROBOTS WIN!")           
+
+
+# EOF
 
